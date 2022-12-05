5a7e8065dcea9b426627d783c45d9ec505644473
Jusley Souza <jusleysouza31@gmail.com>
2022-12-05 19:46:06 -0300

    Implementing upload testing

---
+++ src/test/java/com/ju/rest/fileprocessing/RestfileprocessingApplicationTests.java
@@ -0,0 +1,41 @@
+package com.ju.rest.fileprocessing;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootTest
+class RestfileprocessingApplicationTests {
+	
+	private static final String DOWNLOAD_PATH = "C:/Users/jusle/Documents/Downloads/";
+	private static final String DOWNLOAD_URL = "http://localhost:8080/download/";
+	private static final String FILE_UPLOAD_URL = "http://localhost:8080/upload";
+	
+	@Autowired
+	RestTemplate restTemplate;
+
+	@Test
+	void testUpload() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+		MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+		body.add("file", new ClassPathResource("PP.jpg"));
+		HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<>(body, headers);
+		ResponseEntity<Boolean> response = restTemplate.postForEntity(FILE_UPLOAD_URL, httpEntity, Boolean.class);
+		System.out.println(response.getBody());
+	}
+
+}
